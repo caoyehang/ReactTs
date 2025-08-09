7b6b417e584bbb58f019f136e9946ff8d53fd61a
caoyehang <caoyehang@gmail.com>
2025-08-09 22:14:21 +0800

    feat: 08-09优化

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import Login from "./view/Login/index";
 const App = () => {
-  return <div>app</div>;
+  return (
+    <div>
+      app
+      <Login />
+    </div>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+//
 createRoot(document.getElementById("root")!).render(
-  // 关闭严格模式
+  // 关闭严格模式, 不关闭的话：如果在项目中使用了废弃或已经即将淘汰或已过时的接口语法时候会报错
   // <StrictMode>
   <App />
   // </StrictMode>,
