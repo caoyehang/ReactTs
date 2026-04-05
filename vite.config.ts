c228ba43531b5b026ba375278e889e54cc05ebe5
caoyehang <caoyehang@gamil.com>
2026-04-05 21:42:37 +0800

    perf: 优化配置

--- vite.config.ts
+++ vite.config.ts
@@ -18,8 +18,6 @@ export default defineConfig({
     port: 5173,
     // 项目启动后自动打开浏览器，也可以指定打开某个浏览器
     open: true,
-    //设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口。
-    strictPort: true,
     // 为开发服务器配置 CORS。默认启用并允许任何源，传递一个 选项对象 来调整行为
     cors: true,
   },
